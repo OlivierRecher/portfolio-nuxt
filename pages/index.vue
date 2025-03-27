6ceaf82aa5edac57d74d1cbcd2ee8c48421a162d
OlivierRecher <recher.olivier@outlook.fr>
2025-03-27 21:31:08 +0100

    feat(): maintenance page

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,6 @@ const name = "Olivier Recher";
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
     <h1 class="text-5xl font-bold">{{ name }}</h1>
     <p class="text-xl mt-4">Développeur Web & Ingénieur Informatique</p>
+    <p class="text-xl mt-4">Site en cours de reconstruction</p>
   </div>
 </template>
